a3c5e43a31f556b091e89934cbefcbe95648ef06
allrood <rud.aleksandr.ua@gmail.com>
2016-11-28 18:29:35 +0200

    remake dinamic DOM

--- js_03-04/js/js.js
+++ js_03-04/js/js.js
@@ -1,18 +1,28 @@
-var createDOM = {
-	body: document.body,
-	title: document.creteElement('h4'),
-	div: document.creteElement('div'),
-	form: document.creteElement('form'),
-	button: document.creteElement('button'),
-	createDocument: function () {
-		
-	},
-	
-	createQuestions: function() {
+var wrapper = document.querySelector('.wrapper');
 
-	},
+var title = document.createElement('h1');
+title.innerHTML = 'Тест по программированию';
+wrapper.appendChild(title);
 
-	createButton: function(){
+var form = document.createElement('form');
+wrapper.appendChild(form);
+form.action = 'test';
 
-	}
-};
+var question_1 = document.createElement('div');
+question_1.classList.add('question');
+question_1.innerHTML = '<h2>Вопрос №1 </h2>';
+wrapper.appendChild(question_1);
+
+var question_2 = document.createElement('div');
+question_2.classList.add('question');
+question_2.innerHTML = '<h2>Вопрос №2 </h2>';
+wrapper.appendChild(question_2);
+
+var question_3 = document.createElement('div');
+question_3.classList.add('question');
+question_3.innerHTML = '<h2>Вопрос №3 </h2>';
+wrapper.appendChild(question_3);
+
+var button =  document.createElement('button');
+button.innerHTML = 'проверить';
+wrapper.appendChild(button);
