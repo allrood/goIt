f2118849cc4372b49f9abf81577d104c67d8698b
allrood <rud.aleksandr.ua@gmail.com>
2016-11-29 15:54:56 +0200

    add bootstrap

--- js_03-04/js/js.js
+++ js_03-04/js/js.js
@@ -8,6 +8,8 @@ var form = document.createElement('form');
 wrapper.appendChild(form);
 form.action = 'test';
 
+var lbl = ('label');
+
 var question_1 = document.createElement('div');
 question_1.classList.add('question');
 question_1.innerHTML = '<h2>Вопрос №1 </h2>';
@@ -24,5 +26,81 @@ question_3.innerHTML = '<h2>Вопрос №3 </h2>';
 wrapper.appendChild(question_3);
 
 var button =  document.createElement('button');
-button.innerHTML = 'проверить';
+button.innerHTML = 'Проверить мои результаты';
 wrapper.appendChild(button);
+wrapper.button.classList.add("btn");
+wrapper.button.classList.add("btn-info");
+
+var createDOM = {
+    counter:0,
+    body:document.body,
+    title:document.createElement("h4"),
+    div:document.createElement("div"),
+    form:document.createElement("form"),
+    button:document.createElement("button"),
+    createDocument: function() {
+        this.body.insertBefore(this.div,this.body.children[0]);
+        this.div.classList.add("container");
+        this.div.appendChild(this.title);
+        this.title.innerHTML = "Тест по программированию";
+        this.div.appendChild(this.form);
+        this.form.action = "test";
+    },
+
+    createQuiz:function(questions){
+        console.log(questions);
+        for (var i = 0; i < questions.length; i++ ){
+            var createQuestionDiv = document.createElement("div");
+            createQuestionDiv.classList.add("question");
+            var createQuestionTitle = document.createElement("b");
+            createQuestionTitle.innerHTML = questions[i].question;
+            var createQuestionLabel;
+            var createQuestionCheckbox;
+            this.form.appendChild(createQuestionDiv);
+            createQuestionDiv.appendChild(createQuestionTitle);
+            this.counter++;
+            for (var j = 0; j < questions[i].options.length; j++ ){
+                createQuestionLabel = document.createElement('label');
+                createQuestionLabel.innerHTML = ' ' + questions[i].options[j];
+                createQuestionDiv.appendChild(createQuestionLabel);
+                createQuestionCheckbox = document.createElement("input");
+                createQuestionCheckbox.type = "checkbox";
+                createQuestionCheckbox.setAttribute('id',('question#'+ this.counter + '-answer#' + (j+1)) );
+                createQuestionLabel.insertBefore(createQuestionCheckbox,createQuestionLabel.firstChild);
+            }
+        }
+    },
+    createButton:function(name){
+        this.form.appendChild(this.button);
+        this.button.classList.add("btn");
+        this.button.classList.add("btn-info");
+        this.button.innerHTML = name;
+    }
+};
+
+
+var questionsArray = [
+    {
+        question: 'Вопрос №1',
+        nameOfElements: 'groupAnswers1',
+        options: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+        multiReturn: true
+    },
+    {
+        question: 'Вопрос №2',
+        nameOfElements: 'groupAnswers2',
+        options: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+        multiReturn: true
+    },
+    {
+        question: 'Вопрос №3',
+        nameOfElements: 'groupAnswers3',
+        options: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+        multiReturn: false
+    }
+];
+
+
+createDOM.createDocument();
+createDOM.createQuiz(questionsArray);
+createDOM.createButton('Проверить мои результаты');
