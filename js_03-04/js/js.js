8397ffb418346150a2a9a3f8d6294829f8702f63
allrood <rud.aleksandr.ua@gmail.com>
2016-12-04 02:11:19 +0200

    final commit

--- js_03-04/js/js.js
+++ js_03-04/js/js.js
@@ -13,28 +13,34 @@ newDom = {
         this.div.appendChild(this.form);
     },
     createQuiz: function () {
-        for (var i = 1; i <= 3; i++ ){
+        for (var i = 1; i <= 3; i++) {
             var createQuestionDiv = document.createElement("div");
             createQuestionDiv.classList.add("question");
             var createQuestionTitle = document.createElement("b");
-            createQuestionTitle.innerHTML = i +'. Вопрос №'+ i ;
+            createQuestionTitle.innerHTML = i + '. Вопрос №' + i;
             var createQuestionLabel;
             var createQuestionCheckbox;
             this.form.appendChild(createQuestionDiv);
             createQuestionDiv.appendChild(createQuestionTitle);
-            this.counter++;
-            for (var j = 1; j <= 3; j++ ){
+
+            for (var j = 1; j <= 3; j++) {
                 createQuestionLabel = document.createElement('label');
-                createQuestionLabel.innerHTML = 'Вариант ответа';
+                createQuestionLabel.innerHTML = 'Вариант ответа №' + j;
                 createQuestionDiv.appendChild(createQuestionLabel);
                 createQuestionCheckbox = document.createElement("input");
                 createQuestionCheckbox.type = "checkbox";
-                createQuestionCheckbox.setAttribute('id',('question#'+ this.counter + '-answer#' + (j+1)) );
-                createQuestionLabel.insertBefore(createQuestionCheckbox,createQuestionLabel.firstChild);
+                createQuestionCheckbox.classList.add('answer');
+                createQuestionLabel.insertBefore(createQuestionCheckbox, createQuestionLabel.firstChild);
             }
         }
-
+    },
+    createButton: function () {
+        this.form.appendChild(this.button);
+        this.button.innerHTML = 'Проверить результат';
     }
+
+
 };
 newDom.createDom();
 newDom.createQuiz();
+newDom.createButton();
